96dada40e7221f3560ec992f2dcd0c20a570449e
Noober1 <cucu.ruhiyatna3@gmail.com>
2021-08-31 11:03:32 +0700

    create database connection validator

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const pino = require('pino')
 const pinoHttp = require('pino-http')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const db = require('./lib/db')
 
 module.exports = function main (options, cb) {
+
 	// Set default options
 	const ready = cb || function () {}
 	const opts = Object.assign({
@@ -74,6 +76,7 @@ module.exports = function main (options, cb) {
 	app.use(function fourOhFourHandler (req, res, next) {
 		next(httpErrors(404, `Route not found: ${req.url}`))
 	})
+
 	app.use(function fiveHundredHandler (err, req, res, next) {
 		if (err.status >= 500) {
 			logger.error(err)
@@ -86,10 +89,39 @@ module.exports = function main (options, cb) {
 		})
 	})
 
+	// function for checking database connection
+	const checkDbConnection = async() => {
+		try {
+			await db('dbid')
+			return false;
+		} catch (error) {
+			return {
+				message:'Database connection error, make sure to check database configuration from .env file',
+				code:error.code
+			}
+		}
+	}
+
 	// Start server
-	server = app.listen(opts.port, opts.host, function (err) {
+	server = app.listen(opts.port, opts.host, async function (err) {
+
+		const consoleLogGutter = '==================='
+
+		console.log(consoleLogGutter)
+		console.log('bintar-api starting')
+		console.log(consoleLogGutter)
+
 		if (err) {
 			return ready(err, app, server)
+		}
+
+		//execute database connection check
+		const isError = await checkDbConnection()
+		if (isError) {
+			console.log(isError)
+			return ready(new Error(isError.message))
+		} else {
+			console.log('Database connection: OK')
 		}
 
 		// If some other error means we should close
@@ -99,6 +131,8 @@ module.exports = function main (options, cb) {
 
 		serverStarted = true
 		const addr = server.address()
+		console.log('Web service: OK')
+		console.log(consoleLogGutter)
 		logger.info(`Started at ${opts.host || addr.host || 'localhost'}:${addr.port}`)
 		ready(err, app, server)
 	})

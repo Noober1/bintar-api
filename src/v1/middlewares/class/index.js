8ec947c991e0526ea635704cb545c39f135ca60a
Noober1 <cucu.ruhiyatna3@gmail.com>
2021-11-04 17:32:16 +0700

    fixing data ordering in /v1/class and fixing docs

--- src/v1/middlewares/class/index.js
+++ src/v1/middlewares/class/index.js
@@ -3,8 +3,15 @@ const pagination = require('../../../../lib/pagination')
 const { dataMapping } = require('../../utils')
 
 const getClass = async(req,res,next) => {
+    const { search } = req.query
+    console.log(req.query)
     try {
         const getData = await db('administrasi_kelas_angkatan')
+            .orderBy([
+                {column: 'angkatan', order:'desc'},
+                {column: 'semester', order:'desc'},
+            ])
+            .where('nama', 'like', `%${search || ''}%`)
             .paginate(pagination(req.page,req.limit))
 
         getData.data = getData.data.map(dataMapping.kelas)

628ff4488e5f10df7753e77bec45e3c7198cbef8
Noober1 <cucu.ruhiyatna3@gmail.com>
2022-05-27 21:32:10 +0700

    working on PPDB response API

--- src/v1/controllers/ppdb.js
+++ src/v1/controllers/ppdb.js
@@ -16,46 +16,54 @@ router.route("/login").post(async (req, res, next) => {
         message: "Username dan password harus diisi",
       });
     }
-    const getData = await db("PPDBsiswa").select('id','email','no_telpon').where({
-      email: username,
-      no_telpon: password,
-    }).first();
+    const getData = await db("PPDBsiswa")
+      .select("id", "email", "no_telpon")
+      .where({
+        email: username,
+        no_telpon: password,
+      })
+      .first();
 
     if (getData) {
-      const accessToken = jwt.sign({
-        id: getData.id,
-        email: getData.email
-      }, process.env.ACCESS_TOKEN_SECRET)
+      const accessToken = jwt.sign(
+        {
+          id: getData.id,
+          email: getData.email,
+        },
+        process.env.ACCESS_TOKEN_SECRET
+      );
       return res.json({
         success: true,
-        accessToken: accessToken
-      })
+        accessToken: accessToken,
+      });
     }
-    
+
     throw new sendError({
       code: "ERR_AUTH_INVALID",
       message: "Username atau password salah",
       status: httpStatus.UNAUTHORIZED,
-    })
+    });
   } catch (error) {
     next(error);
   }
 });
 
-router
-  .route('/profile')
-  .get(
-    withAuthToken,
-    async (req, res, next) => {
-      try {
-        console.log(req.auth)
-        const getData = await db("PPDBsiswa").where('id', req.auth.id).first()
-    
-        return res.json(getData)
-      } catch (error) {
-        next(error)
-      }
-    }
-  )
+router.route("/profile").get(withAuthToken, async (req, res, next) => {
+  try {
+    console.log(req.auth);
+    const getData = await db("PPDBsiswa")
+      .select("nama_depan", "nama_belakang", "email")
+      .where("id", req.auth.id)
+      .first();
+
+    return res.json({
+      firstName: getData.nama_depan,
+      lastName: getData.nama_belakang,
+      fullName: getData.nama_depan + " " + getData.nama_belakang,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
